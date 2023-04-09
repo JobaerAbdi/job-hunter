6678faac879a18fde90f907c4c7fd7d53bff4c0d
JobaerAbdi <jobaerrahman757@gmail.com>
2023-04-09 22:45:17 +0600

    Navbar section complete

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100 shadow-lg lg:px-12">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 font-semibold"
+            >
+              <Link to="/">
+                <li>
+                  <a>Home</a>
+                </li>
+              </Link>
+              <Link to="/statistics">
+                <li>
+                  <a>Statistics</a>
+                </li>
+              </Link>
+              <Link to="/jobs">
+                <li>
+                  <a>Applied Jobs</a>
+                </li>
+              </Link>
+              <Link to="/blogs">
+                <li>
+                  <a>Blogs</a>
+                </li>
+              </Link>
+            </ul>
+          </div>
+          <a className="btn btn-ghost normal-case font-bold text-2xl">
+            Job Hunter
+          </a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 font-bold">
+            <li className="mr-9">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive && "bg-cyan-300 text-black px-3 py-1 rounded-md"
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="mr-9">
+              <NavLink
+                to="/statistics"
+                className={({ isActive }) =>
+                  isActive && "bg-cyan-300 text-black px-3 py-1 rounded-md"
+                }
+              >
+                Statistics
+              </NavLink>
+            </li>
+            <li className="mr-9">
+              <NavLink
+                to="/jobs"
+                className={({ isActive }) =>
+                  isActive && "bg-cyan-300 text-black px-3 py-1 rounded-md"
+                }
+              >
+                Applied Jobs
+              </NavLink>
+            </li>
+            <li className="mr-9">
+              <NavLink
+                to="/blogs"
+                className={({ isActive }) =>
+                  isActive && "bg-cyan-300 text-black px-3 py-1 rounded-md"
+                }
+              >
+                Blogs
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Apply Now</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './components/Layout/Layout'
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Layout></Layout>
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
